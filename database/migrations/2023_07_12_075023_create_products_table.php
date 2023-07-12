27349511911d83990cc2790309491778a7e45a8c
PovilasKorop <povilask007@gmail.com>
2023-07-12 15:03:03 +0300

    Small fix: no need restaurant field in products

--- database/migrations/2023_07_12_075023_create_products_table.php
+++ database/migrations/2023_07_12_075023_create_products_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\Category;
-use App\Models\Restaurant;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +14,6 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Restaurant::class)->constrained();
             $table->foreignIdFor(Category::class)->constrained();
             $table->string('name');
             $table->unsignedInteger('price');
